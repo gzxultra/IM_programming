b88715fe814dbb4cd06918c54ffec375d44ecaf4
gzxultra <gladfinger@emails.bjut.edu.cn>
2015-06-25 14:10:02 +0800

    6.25，ump通信
    实现了简单的udpC/S，客户端发送消息，服务器端接受消息并显示socket信息

---
+++ server_byUDP.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+from socket import *
+from time import ctime
+
+HOST = '127.0.0.1'
+PORT = 21567
+BUFSIZE = 1024
+
+ADDR = (HOST,PORT)
+
+udpSerSock = socket(AF_INET, SOCK_DGRAM)
+# udpSerSock.setblocking(0)
+# udpSerSock.settimeout(0.5)
+udpSerSock.bind(ADDR)
+
+while True:
+    print 'wating for message...'
+    data, addr = udpSerSock.recvfrom(BUFSIZE)
+    udpSerSock.sendto('[%s] %s'%(ctime(),data),addr)
+    print '...received from and retuned to:',addr
+
+udpSerSock.close()
